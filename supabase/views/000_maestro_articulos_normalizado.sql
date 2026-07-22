335d5f45f42dcfb8f37a8be70dbc6be4c869c0c0
LOCOSLOS <pablo@loslocos.com.ar>
2026-07-22 13:56:26 -0300

    tratar variantes como opcionales

--- supabase/views/000_maestro_articulos_normalizado.sql
+++ supabase/views/000_maestro_articulos_normalizado.sql
@@ -1,6 +1,7 @@
 -- Vista normalizada del maestro de artículos.
 -- No modifica la tabla de origen. Conserva la descripción original y separa
--- color y talle cuando respeta el patrón "... C:<color> T:<talle>".
+-- color y talle cuando existen los marcadores correspondientes. Ambos son
+-- opcionales y pueden aparecer en cualquier orden.
 
 create or replace view public.vw_maestro_articulos_normalizado
 with (security_invoker = true)
@@ -13,33 +14,44 @@ select
   m.rubro,
   m.subrubro,
   case
-    when m.descripcion like '% C:% T:%'
+    when m.descripcion like '% C:%'
       then nullif(
-        btrim(split_part(split_part(m.descripcion, ' C:', 2), ' T:', 1)),
+        btrim(
+          split_part(
+            split_part(m.descripcion, ' C:', 2),
+            ' T:',
+            1
+          )
+        ),
         ''
       )
     else null
   end as color,
   case
-    when m.descripcion like '% C:% T:%'
-      then nullif(btrim(split_part(m.descripcion, ' T:', 2)), '')
+    when m.descripcion like '% T:%'
+      then nullif(
+        btrim(
+          split_part(
+            split_part(m.descripcion, ' T:', 2),
+            ' C:',
+            1
+          )
+        ),
+        ''
+      )
     else null
   end as talle,
   case
     when m.descripcion is null or btrim(m.descripcion) = '' then 'sin_descripcion'
-    when m.descripcion not like '% C:%' then 'sin_color'
-    when m.descripcion not like '% T:%' then 'sin_talle'
-    when nullif(
-      btrim(split_part(split_part(m.descripcion, ' C:', 2), ' T:', 1)),
-      ''
-    ) is null then 'color_vacio'
-    when nullif(btrim(split_part(m.descripcion, ' T:', 2)), '') is null
-      then 'talle_vacio'
-    else 'ok'
+    when m.descripcion like '% C:%'
+      and m.descripcion like '% T:%' then 'con_color_y_talle'
+    when m.descripcion like '% C:%' then 'solo_color'
+    when m.descripcion like '% T:%' then 'solo_talle'
+    else 'sin_variantes_declaradas'
   end as estado_parseo_variante,
   m.updated_at
 from centum_sync.maestro_articulos m;
 
 comment on view public.vw_maestro_articulos_normalizado is
-  'Maestro de Centum enriquecido con color y talle extraídos de la descripción, conservando el texto original y el estado del parseo.';
+  'Maestro de Centum enriquecido con color y talle opcionales extraídos de la descripción, conservando el texto original y una clasificación descriptiva.';
 
